1b1d239e850203adc656ffb01fe9b5e8191e5be2
vasatile <vasatileucee@gmail.com>
2024-09-24 22:22:42 +0100

    logs

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from mutes import Mutes
 from twitterbot import TwitterBot
 import time
 import json
-import sys
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Your Twitter API credentials
 API_KEY = os.getenv('TWITTER_API_KEY')
@@ -20,7 +23,6 @@ ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 # Create an instance of TwitterBot
 bot = TwitterBot(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-
 # Load the ENS names from the JSON file with utf-8 encoding
 with open('noteworthyens.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
@@ -28,13 +30,10 @@ with open('noteworthyens.json', 'r', encoding='utf-8') as file:
 # Access the ENS names list
 addressList = data['ens_names']
 
-
-
 # Lists to hold new entries
 new_followings = []
 new_blockings = []
 new_muting = []
-
 
 def process_followings(address):
     efp_user = FollowAndTags(address)
@@ -46,12 +45,11 @@ def process_followings(address):
         if addresses:
             new_addresses = addresses[0]  # Get the last element
             new_followings.append({address: new_addresses})
-            print(f"New followings found for {address}: {new_addresses}")
+            logging.info(f"New followings found for {address}: {new_addresses}")
         else:
-            # new_followings.append({address: []})
-            print(f"No new followings found for {address}")
+            logging.info(f"No new followings found for {address}")
     except (requests.HTTPError, IndexError) as e:
-        print(f"Error occurred while processing followings for {address}: {e}")
+        logging.error(f"Error occurred while processing followings for {address}: {e}")
 
 def process_blockings(address):
     efp_user = FollowAndTags(address)
@@ -66,12 +64,11 @@ def process_blockings(address):
         if listofBlockusers:
             new_blocked_addresses = listofBlockusers[0]  # Get the last element
             new_blockings.append({address: new_blocked_addresses})
-            print(f"New blockings found for {address}: {new_blocked_addresses}")
+            logging.info(f"New blockings found for {address}: {new_blocked_addresses}")
         else:
-            # new_blockings.append({address: []})
-            print(f"No new blockings found for {address}")
+            logging.info(f"No new blockings found for {address}")
     except (requests.HTTPError, IndexError) as e:
-        print(f"Error occurred while processing blockings for {address}: {e}")
+        logging.error(f"Error occurred while processing blockings for {address}: {e}")
 
 def process_mutings(address):
     efp_user = FollowAndTags(address)
@@ -86,86 +83,57 @@ def process_mutings(address):
         if listofMutedusers:
             new_muted_addresses = listofMutedusers[0]  # Get the last element
             new_muting.append({address: new_muted_addresses})
-            print(f"New mutings found for {address}: {new_muted_addresses}")
+            logging.info(f"New mutings found for {address}: {new_muted_addresses}")
         else:
-            # new_muting.append({address: []})
-            print(f"No new mutings found for {address}")
-            
+            logging.info(f"No new mutings found for {address}")
     except (requests.HTTPError, IndexError) as e:
-        print(f"Error occurred while processing mutings for {address}: {e}")
+        logging.error(f"Error occurred while processing mutings for {address}: {e}")
 
-
-
-        # Process new followings
 def process_new_entries(new_followings, new_blockings, new_muting, bot):
     # Process new followings
     if new_followings:
         for new_entry in new_followings:
             for address, addresses in new_entry.items():
                 if addresses:
-                    print(addresses)
                     ensurl = EnsUrl(addresses)
                     get_ensurl = ensurl.get_ens_data_url()
                     fetchens = FetchEns(get_ensurl)
                     name = fetchens.extract_ens()
-                    print(f"here is {name}")
                     if name == "":
-                        print("no name")
+                        logging.warning(f"No ENS name found for address {address}")
                         continue
                     
                     bot.post_following_tweet(address, name)
-                else:
-                    print("no address in list")
-                
-    else:
-        pass
-                
-                
 
     # Process new blockings
     if new_blockings:
         for new_entry in new_blockings:
             for address, addresses in new_entry.items():
                 if addresses:
-                        ensurl = EnsUrl(addresses)
-                        get_ensurl = ensurl.get_ens_data_url()
-                        fetchens = FetchEns(get_ensurl)
-                        name = fetchens.extract_ens()
-                        if name =="":
-                            print("name is empty")
-                            continue
-                        
-                        bot.post_blocking_tweet(address, name)
-                else:
-                    print("addresses is empty")
-                            
-                        
-                    
-                    
-    else:
-        print("nothing to check") 
-
-   # Process new mutings
-    if new_muting:  # Check if new_muting is not empty
-        for new_entry in new_muting:
-            for address, addresses in new_entry.items():
-                if addresses:
-    
                     ensurl = EnsUrl(addresses)
                     get_ensurl = ensurl.get_ens_data_url()
                     fetchens = FetchEns(get_ensurl)
                     name = fetchens.extract_ens()
-                    if name =="" or name is None:
-                        print("no name here")
+                    if name == "":
+                        logging.warning(f"No ENS name found for blocking address {address}")
                         continue
                     
-                    bot.post_muting_tweet(address, name)  
-                else: 
-                    print("no addresses")           
-                        
-    else:
-        print("nothing to check")
+                    bot.post_blocking_tweet(address, name)
 
+    # Process new mutings
+    if new_muting:  
+        for new_entry in new_muting:
+            for address, addresses in new_entry.items():
+                if addresses:
+                    ensurl = EnsUrl(addresses)
+                    get_ensurl = ensurl.get_ens_data_url()
+                    fetchens = FetchEns(get_ensurl)
+                    name = fetchens.extract_ens()
+                    if name == "" or name is None:
+                        logging.warning(f"No ENS name found for muting address {address}")
+                        continue
+                    
+                    bot.post_muting_tweet(address, name)
 
 # Call the function
 while True:
@@ -174,9 +142,6 @@ while True:
         process_blockings(address)
         process_mutings(address)
         
-    
-    
-
     # Call the function to process new entries
     process_new_entries(new_followings, new_blockings, new_muting, bot)
 
