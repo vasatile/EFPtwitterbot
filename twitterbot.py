0a016d118b487cc1ec16946e9749bf7e5bde216d
vasatile <vasatileucee@gmail.com>
2024-09-24 18:10:46 +0100

    message changes

--- twitterbot.py
+++ twitterbot.py
@@ -35,34 +35,40 @@ class TwitterBot:
     def post_following_tweet(self, address, ens_name):
         """Post a tweet about a new follower."""
         tweet =  f"""
-        🚨 NEW ALERT!! on Eth Follow Protocol
+        
         
     {address} started following {ens_name}
 
-        Track and Follow wallets on https://ethfollow.xyz/
+        Track and Follow wallets @efp
+        
+        https://ethfollow.xyz/{address}
         """
         self.post_tweet(tweet)
 
     def post_blocking_tweet(self, address, ens_name):
         """Post a tweet about a new blocker."""
         tweet = f"""
-        🚨 NEW ALERT!! on Eth Follow Protocol 
+         
         
     {address} just blocked {ens_name}
         
         Bad wallet maybe 🤐
         
-        Track and Follow wallets https://ethfollow.xyz/"""
+        Track and Follow wallets @efp
+        
+        https://ethfollow.xyz/{address}"""
         self.post_tweet(tweet)
 
     def post_muting_tweet(self, address, ens_name):
         """Post a tweet about a new mute."""
         tweet = f"""
-        🚨 NEW ALERT!! on Eth Follow Protocol
+       
         
     {address} just muted {ens_name}
         
         🤐Looks like {ens_name} is Good at buying the Top
         
-        Track and Follow wallets https://ethfollow.xyz/"""
+        Track and Follow wallets @efp
+        
+        https://ethfollow.xyz/{address}"""
         self.post_tweet(tweet)
